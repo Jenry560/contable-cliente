daaa477c76da17bb89571de22ffb1c5625d98068
Jenry Sanchez <henryfuerte560@gmail.com>
2024-02-25 16:14:09 -0400

    nueva actulizacion de estetica 1.0

--- src/componetes/modulos.jsx
+++ src/componetes/modulos.jsx
@@ -1,154 +1,133 @@
-import { Link } from 'react-router-dom';
-import '../App.css'
-const cliente = new URL("../imagenes/cliente.png",import.meta.url);
-const registro = new URL("../imagenes/registro1.png",import.meta.url);
-const cxc = new URL("../imagenes/cxc.jpg",import.meta.url);
-const cxp = new URL("../imagenes/cxp.png",import.meta.url);
-const ingresos = new URL("../imagenes/ingresos.png",import.meta.url);
-const invetario = new URL("../imagenes/inve2.png",import.meta.url);
-const gasto = new URL("../imagenes/gastos.png",import.meta.url);
-const diario = new URL("../imagenes/diario.png",import.meta.url);
-const Proveedores = new URL("../imagenes/proveedores.png",import.meta.url);
+import { Link } from "react-router-dom";
+import "../App.css";
+const cliente = new URL("../imagenes/cliente.png", import.meta.url);
+const registro = new URL("../imagenes/registro1.png", import.meta.url);
+const cxc = new URL("../imagenes/billete.jpg", import.meta.url);
+const cxp = new URL("../imagenes/cuentaPorPagar.png", import.meta.url);
+const ingresos = new URL("../imagenes/ingresos.png", import.meta.url);
+const invetario = new URL("../imagenes/inve2.png", import.meta.url);
+const gasto = new URL("../imagenes/GastoNew.png", import.meta.url);
+const diario = new URL("../imagenes/Mayor.png", import.meta.url);
+const Proveedores = new URL("../imagenes/proveedores.png", import.meta.url);
 function Modulos() {
-
   return (
     <>
-    <div className='container'>
-    
-     <div className="picture__cont">
-       <div >
-       <Link to="/cliente" >
-       <figure className="information__pictures">
-            <img src={cliente} className="information__img" />
-            <div className="capa">
-              <p className="abs">Clientes</p>
-            </div>    
-        </figure>
-       </Link>
-        <h1 className='title'>Clientes</h1>
+      <div className="container">
+        <div className="picture__cont">
+          <div>
+            <Link to="/cliente">
+              <figure className="information__pictures">
+                <img src={cliente} className="information__img" />
+                <div className="capa">
+                  <p className="abs">Clientes</p>
+                </div>
+              </figure>
+            </Link>
+            <h1 className="title">Clientes</h1>
+          </div>
+
+          <div>
+            <Link to="/proveedores">
+              <figure className="information__pictures">
+                <img src={Proveedores} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">Proveedores</p>
+                </div>
+              </figure>
+            </Link>
+
+            <h1 className="title">Proveedores</h1>
+          </div>
+
+          <div>
+            <Link to="/registro">
+              <figure className="information__pictures">
+                <img src={registro} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">Registro</p>
+                </div>
+              </figure>
+            </Link>
+
+            <h1 className="title">Registro</h1>
+          </div>
+          <div>
+            <Link to="/cxc">
+              <figure className="information__pictures">
+                <img src={cxc} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">CXC</p>
+                </div>
+              </figure>
+            </Link>
+
+            <h1 className="title">Cuenta por Cobrar</h1>
+          </div>
+
+          <div>
+            <Link to="/cxp">
+              <figure className="information__pictures">
+                <img src={cxp} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">7060</p>
+                </div>
+              </figure>
+            </Link>
+
+            <h1 className="title">Cuenta por pagar</h1>
+          </div>
+
+          <div>
+            <Link to="/ingresos">
+              <figure className="information__pictures">
+                <img src={ingresos} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">7070</p>
+                </div>
+              </figure>
+            </Link>
+            <h1 className="title">Ingresos</h1>
+          </div>
+
+          <div>
+            <Link to="/gastos">
+              <figure className="information__pictures">
+                <img src={gasto} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">7080</p>
+                </div>
+              </figure>
+            </Link>
+            <h1 className="title">Gastos</h1>
+          </div>
+
+          <div>
+            <Link to="/inventario">
+              <figure className="information__pictures">
+                <img src={invetario} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">7090</p>
+                </div>
+              </figure>
+            </Link>
+            <h1 className="title">Inventario</h1>
+          </div>
+
+          <div>
+            <Link to="/diario">
+              <figure className="information__pictures">
+                <img src={diario} className="information__img" />
+                <div className="capa">
+                  <p className="absolute">8000</p>
+                </div>
+              </figure>
+            </Link>
+            <h1 className="title">Mayor general</h1>
+          </div>
         </div>
-
-
-        <div>
-        <Link to="/proveedores" >
-        <figure className="information__pictures">
-            <img src={Proveedores} className="information__img" />
-            <div className="capa">
-              <p className="absolute">Proveedores</p>
-             
-              
-            </div>
-        </figure>
-        </Link>
-        
-        <h1 className='title'>Proveedores</h1>
-        </div> 
-        
-        <div  >
-        <Link to="/registro" >
-        <figure className="information__pictures">
-            <img src={registro} className="information__img" />
-            <div className="capa">
-              <p className="absolute">Registro</p>
-           
-            </div>
-        </figure>
-        </Link>
-        
-        <h1 className='title'>Registro</h1>
-        </div> 
-        <div  >
-        <Link to="/cxc" >
-        <figure className="information__pictures">
-            <img src={cxc} className="information__img" />
-            <div className="capa">
-              <p className='absolute'>CXC</p>
-              
-            </div>
-        </figure>
-        </Link>   
-       
-        <h1 className='title'>Cuenta por Cobrar</h1>
-        </div> 
-        
-        <div  >
-        <Link to="/cxp" >
-        <figure className="information__pictures">
-            <img src={cxp} className="information__img" />
-            <div className="capa">
-              <p className="absolute">7060</p>
-              
-              
-            </div>
-        </figure>
-        </Link>
-       
-        <h1 className='title'>Cuenta por pagar</h1>
-        </div> 
-
-        <div  >
-        <Link to="/ingresos" >
-        <figure className="information__pictures">
-            <img src={ingresos} className="information__img" />
-            <div className="capa">
-            <p className="absolute">7070</p>              
-            </div>
-        </figure>
-        </Link>
-        <h1 className='title'>Ingresos</h1>
-        </div> 
-
-        <div  >
-        <Link to="/gastos" >
-        <figure className="information__pictures">
-            <img src={gasto} className="information__img" />
-            <div className="capa">
-            <p className="absolute">7080</p>
-              
-              
-            </div>
-        </figure>
-        </Link>
-        <h1 className='title'>Gastos</h1>
-        </div> 
-
-        <div  >
-        <Link to="/inventario">
-        <figure className="information__pictures">
-            <img src={invetario} className="information__img" />
-            <div className="capa">
-            <p className="absolute">7090</p>
-             
-              
-            </div>
-        </figure>
-        </Link>
-        <h1 className='title'>Inventario</h1>
-        </div> 
-
-        <div  >
-        <Link to="/diario" >
-        <figure className="information__pictures">
-            <img src={diario} className="information__img" />
-            <div className="capa">
-            <p className="absolute">8000</p>
-              
-              
-            </div>
-        </figure>
-        </Link>
-        <h1 className='title'>Mayor general</h1>
-        </div> 
-        
-       
-
-       
-       
-     </div>
-        </div>
-   
+      </div>
     </>
-  )
+  );
 }
 
-export default Modulos
+export default Modulos;
--- src/rutas.jsx
+++ src/rutas.jsx
@@ -1,66 +1,120 @@
-import React, { useEffect, useState } from 'react'
-
-import App from './App.jsx'
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from "react-router-dom";
-import Cliente from './componetes/modulos/Cliente componentes/clientes.jsx'
-import Registro from './componetes/modulos/registro.jsx'
-import Modelo from './componetes/modulos/modelo.jsx'
+import App from "./App.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cliente from "./componetes/modulos/Cliente componentes/clientes.jsx";
+import Registro from "./componetes/modulos/registro.jsx";
+import Modelo from "./componetes/modulos/modelo.jsx";
 import { TbPigMoney } from "react-icons/tb";
 import { BsBank2 } from "react-icons/bs";
-import { TfiMoney} from "react-icons/tfi";
-import { GiPayMoney} from "react-icons/gi";
-import { BsFillHouseAddFill} from "react-icons/bs";
-import { RiNewspaperLine} from "react-icons/ri";
-import Diario from './componetes/modulos/Diario.jsx'
-import Cobrar from './componetes/modulos/cobrar.jsx'
-import Sign from './componetes/user/sign.jsx'
-import Log from './componetes/user/log.jsx'
-import Modulos from './componetes/modulos.jsx'
-import ProtectedRoute from './componetes/utils/protect.jsx'
-import { useLocalStorage } from 'react-use'
-import Principal from './componetes/principal/principal.jsx';
-import Proveedor from './componetes/modulos/Proveedor componenes/proveedor.jsx';
-import PorPagar from './componetes/modulos/porPagar.jsx';
+import { TfiMoney } from "react-icons/tfi";
+import { GiPayMoney } from "react-icons/gi";
+import { BsFillHouseAddFill } from "react-icons/bs";
+import { RiNewspaperLine } from "react-icons/ri";
+import Diario from "./componetes/modulos/Diario.jsx";
+import Cobrar from "./componetes/modulos/cobrar.jsx";
+import Sign from "./componetes/user/sign.jsx";
+import Log from "./componetes/user/log.jsx";
+import Modulos from "./componetes/modulos.jsx";
+import ProtectedRoute from "./componetes/utils/protect.jsx";
+import { useLocalStorage } from "react-use";
+import Principal from "./componetes/principal/principal.jsx";
+import Proveedor from "./componetes/modulos/Proveedor componenes/proveedor.jsx";
+import PorPagar from "./componetes/modulos/porPagar.jsx";
 
+export default function Rutas() {
+  const [user] = useLocalStorage("user");
 
-
-
-export default function Rutas(){
-
-    const [user,setUser]= useLocalStorage('user')
-
-    
-
-    return(
-     <Router>
+  return (
+    <Router>
       <Routes>
-        <Route element={<ProtectedRoute canActivate={user} redirectPath={"/signs"}/>}>
-        <Route path='/' element={<App/>}>
-          <Route path='/' element={<Modulos/>} />
-          <Route path='cliente' element={<Cliente RegistradoPor={user && user.Correo}/>}/>
-          <Route path='Proveedores' element={<Proveedor RegistradoPor={user && user.Correo}/>}/>
-          <Route path='registro' element={<Registro RegistradoPor={user && user.Correo}/>}/>
-          <Route path='cxc' element={<Cobrar title="Cuentas por cobrar" codigo="CXC" RegistradoPor={user && user.Correo} icon={<TbPigMoney className='ite' />} />}/>
-          <Route path='cxp' element={<PorPagar title="Cuentas por pagar" codigo="7060" RegistradoPor={user && user.Correo} icon={<BsBank2 className='ite' />}/>}/>
-          <Route path='ingresos' element={<Modelo title="Ingresos" codigo="7070" RegistradoPor={user && user.Correo} icon={<TfiMoney className='ite' />}/>}/>
-          <Route path='gastos' element={<Modelo title="Gastos" codigo="7080" RegistradoPor={user && user.Correo} icon={<GiPayMoney className='ite'  />}/>}/>
-          <Route path='inventario' element={<Modelo title="Inventario" codigo="7090" RegistradoPor={user && user.Correo} icon={<BsFillHouseAddFill  className='ite' />}/>}/>
-          <Route path='diario' element={<Diario RegistradoPor={user && user.Correo} icon={<RiNewspaperLine className='ite'  />}/>}/>
+        <Route
+          element={
+            <ProtectedRoute canActivate={user} redirectPath={"/signs"} />
+          }
+        >
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Modulos />} />
+            <Route
+              path="cliente"
+              element={<Cliente RegistradoPor={user && user.Correo} />}
+            />
+            <Route
+              path="Proveedores"
+              element={<Proveedor RegistradoPor={user && user.Correo} />}
+            />
+            <Route
+              path="registro"
+              element={<Registro RegistradoPor={user && user.Correo} />}
+            />
+            <Route
+              path="cxc"
+              element={
+                <Cobrar
+                  title="Cuentas por cobrar"
+                  codigo="CXC"
+                  RegistradoPor={user && user.Correo}
+                  icon={<TbPigMoney className="ite" />}
+                />
+              }
+            />
+            <Route
+              path="cxp"
+              element={
+                <PorPagar
+                  title="Cuentas por pagar"
+                  codigo="7060"
+                  RegistradoPor={user && user.Correo}
+                  icon={<BsBank2 className="ite" />}
+                />
+              }
+            />
+            <Route
+              path="ingresos"
+              element={
+                <Modelo
+                  title="Ingresos"
+                  codigo="7070"
+                  RegistradoPor={user && user.Correo}
+                  icon={<TfiMoney className="ite" />}
+                />
+              }
+            />
+            <Route
+              path="gastos"
+              element={
+                <Modelo
+                  title="Gastos"
+                  codigo="7080"
+                  RegistradoPor={user && user.Correo}
+                  icon={<GiPayMoney className="ite" />}
+                />
+              }
+            />
+            <Route
+              path="inventario"
+              element={
+                <Modelo
+                  title="Inventario"
+                  codigo="7090"
+                  RegistradoPor={user && user.Correo}
+                  icon={<BsFillHouseAddFill className="ite" />}
+                />
+              }
+            />
+            <Route
+              path="diario"
+              element={
+                <Diario
+                  RegistradoPor={user && user.Correo}
+                  icon={<RiNewspaperLine className="ite" />}
+                />
+              }
+            />
+          </Route>
         </Route>
-        </Route>
-        <Route path='signs' element={<Principal/>}/>
-       <Route path='log' element={<Log/>}/>
-       <Route path='sign' element={<Sign/>}/>        
+        <Route path="signs" element={<Principal />} />
+        <Route path="log" element={<Log />} />
+        <Route path="sign" element={<Sign />} />
       </Routes>
     </Router>
- 
-    )
+  );
 }
-  
-
